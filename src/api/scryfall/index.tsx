02652f7a869c4173f3a83a1e754212173118469a
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-09-24 14:03:19 +0200

    Add if  checks for double faced cards.

--- src/api/scryfall/index.tsx
+++ src/api/scryfall/index.tsx
@@ -6,7 +6,12 @@ interface Scryfall {
 		type_line: string
 		oracle_text: string
 		image_uris: {
-			normal: string
+			normal: string | undefined
+		}
+		card_faces: {
+			image_uris: {
+				normal: string | undefined
+			}
 		}
 	}[]
 }
@@ -16,7 +21,10 @@ const Scryfall = () => {
 	const [name, setName] = useState<string | null>(null)
 	const [type, setType] = useState<string | null>(null)
 	const [text, setText] = useState<string | null>(null)
-	const [img, setImg] = useState<string>("")
+	// const [doubleFacedCardImg, setDoubleFacedCardImg] = useState<
+	// 	string | undefined
+	// >("")
+	const [img, setImg] = useState<string | undefined>("")
 	const [searchValue, setSearchValue] = useState<string>("obsessive search")
 
 	const apiFetch = async () => {
@@ -31,7 +39,22 @@ const Scryfall = () => {
 				setName(jsonData.data[0].name)
 				setType(jsonData.data[0].type_line)
 				setText(jsonData.data[0].oracle_text)
-				setImg(jsonData.data[0].image_uris.normal)
+
+				if (
+					jsonData.data[0].image_uris &&
+					jsonData.data[0].image_uris.normal !== undefined
+				) {
+					setImg(jsonData.data[0].image_uris.normal)
+				} else if (
+					jsonData.data[0].card_faces &&
+					jsonData.data[0].card_faces[0].image_uris &&
+					jsonData.data[0].card_faces[0].image_uris.normal !==
+						undefined
+				) {
+					setImg(jsonData.data[0].card_faces[0].image_uris.normal)
+				} else {
+					setImg("") // Set to empty string if no image URL is found
+				}
 			}
 		} catch (error) {
 			console.error("Error fetching data:", error)
@@ -66,13 +89,15 @@ const Scryfall = () => {
 			{data ? <div>Namn: {name}</div> : <p>Loading...</p>}
 			{data ? <div>Typ: {type}</div> : <p>Loading...</p>}
 			{data ? <div>Text: {text}</div> : <p>Loading...</p>}
-			{data ? (
-				<div>
-					<img src={img} />
-				</div>
-			) : (
-				<p>Loading...</p>
-			)}
+			<div>
+				{data && img !== "" ? (
+					<div>
+						<img src={img} />
+					</div>
+				) : (
+					<p>No image could be loaded</p>
+				)}
+			</div>
 		</div>
 	)
 }
